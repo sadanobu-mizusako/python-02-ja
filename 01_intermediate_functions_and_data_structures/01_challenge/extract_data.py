ed425921f78e38d5fe70a5ceb18d2915578b4ad6
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-05-14 07:23:11 +0900

    add answer for challange 01

--- 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
+++ 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
@@ -1,7 +1,25 @@
 import re
 
 def extract_data(text):
-    pass
+    pattern = r'[a-zA-Z0-9_]+\:[a-zA-Z0-9_]+'
+    matches = re.findall(pattern, text)
+    return matches
 
 def better_extract_data(text):
-    pass
+    pattern = re.compile(r'([a-zA-Z0-9_]+)\:([a-zA-Z0-9_]+)')
+    matches = extract_data(text)
+
+    res = []
+    for m in matches:
+        res_ = pattern.search(m)
+        res.append((res_.group(1), res_.group(2)))
+
+    return res
+
+if __name__ == "__main__":
+    text = "The subject has Age:25 and Height:180cm.Other details are not relevant.Weight:70kg was noted."
+    matches = extract_data(text)
+    print(matches) 
+
+    res = better_extract_data(text)
+    print(res) 
