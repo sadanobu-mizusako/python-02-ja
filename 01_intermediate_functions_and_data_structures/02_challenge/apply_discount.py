68b32b66c73ed7bb48a664c69a340f20537fc254
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-05-17 14:50:18 +0900

    live coding version

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,5 +1,12 @@
 def apply_discount(products, minimum_price, discount_rate):
     # return [(p["name"], p["price"]*(1-discount_rate/100)) 
     #         for p in products if p["price"]*(1-discount_rate/100)>=minimum_price]
-    products_tupple = [(p["name"], p["price"]*(1-discount_rate/100)) for p in products]
-    return list(filter(lambda p: p[1]>=minimum_price, products_tupple))
+    # products_tupple = [(p["name"], apply_discount(p["price"])) for p in products]
+    # return list(filter(lambda p: p[1]>=minimum_price, products_tupple))
+
+    apply_discount = lambda price: price *(1-discount_rate/100)
+    return [
+        (elem["name"], apply_discount(elem["price"])) 
+        for elem in products
+        if elem["price"] >= minimum_price
+    ]
