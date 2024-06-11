02c1e08eae212dce0c30d3320d305d90813af9d4
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-06-11 20:32:52 +0900

    commit for 04/01

--- 04_error_handling_and_debugging/01_challenge/mystery_function.py
+++ 04_error_handling_and_debugging/01_challenge/mystery_function.py
@@ -1,6 +1,6 @@
 def mystery_function(lst):
     result = lst
     for i in range(len(lst)):
-        if i % 2 == 0:
-            result[i] = i ** 2
+        if lst[i] % 2 == 0:
+            result[i] = lst[i] ** 2
     return result
